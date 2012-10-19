782e1cfe5ec69015da4d1f8d53759ad66574b2f6
Nicolas Rémond <nremond+github@gmail.com>
2012-10-19 23:26:37 +0200

    I can display a list

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+
+	def index = Action {
+
+		val freshNews = List(
+			News(1l, "Apple are better than pears", "http://lemonde.fr"), News(2l, "Secutix may sell you tix", "http://secutix.com"))
+
+		Ok(views.html.index("Your new application is ready.", freshNews))
+	}
+
 }
